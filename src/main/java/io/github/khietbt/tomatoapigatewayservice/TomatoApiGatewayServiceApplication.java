304fe036e2114013e4fd3e23174e6e5cec30de98
khietbt <khietbt@clevai.edu.vn>
2023-02-20 15:54:58 +0700

    Initial commit

---
+++ src/main/java/io/github/khietbt/tomatoapigatewayservice/TomatoApiGatewayServiceApplication.java
@@ -0,0 +1,11 @@
+package io.github.khietbt.tomatoapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoApiGatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TomatoApiGatewayServiceApplication.class, args);
+    }
+}
